3809355f5ca05d17e294dc912265256d2fa0c5fc
Sayem2004 <mdalimransayem@gmail.com>
2026-07-29 13:34:27 +0600

    Complete user registration and validation

--- fresh-food-backend/src/users/users.controller.ts
+++ fresh-food-backend/src/users/users.controller.ts
@@ -1,9 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 
+import { Body, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get()
   getUsers() {
     return 'Users API is working';
+  }
+
+  @Post('register')
+  register(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.register(createUserDto);
   }
 }
